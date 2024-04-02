044164f012f9b3cb104b1a8a5d511c52c693d186
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-04-02 12:53:39 -0400

    new file:   me/gay.css 	new file:   me/gay.html 	modified:   me/me.css 	modified:   me/me.html 	modified:   me/me.js

--- me/me.js
+++ me/me.js
@@ -7,10 +7,15 @@ const maxHeight = window.innerHeight - buttonHeight;
 window.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('btn');
 
-    button.addEventListener('click', () => alert('You clicked me!'));
+    
 
     button.addEventListener('mouseover', () => {
         button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
         button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
+    });
+
+    const button1 = document.getElementById('yes');
+    button1.addEventListener('click', () => {
+        window.location.href = "gay.html";
     });
 });
