18e1514acecc9321efe09fcb631ef75eee07bf92
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-05-07 11:18:27 -0400

    new file:   scrimba/index.css 	new file:   scrimba/index.html 	new file:   scrimba/index.js

---
+++ scrimba/index.js
@@ -0,0 +1,11 @@
+let count = 0
+let countEl = document.getElementById("count-el")
+
+function increment(){
+    count = count + 1
+    countEl.innerText = count 
+}
+
+function save(){
+    console.log("count")
+}
